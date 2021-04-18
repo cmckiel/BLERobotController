19f9501bcd4833dfd0b3eb8d4a20d14b36c24183
Cory <cjm48d@mail.umsl.edu>
2021-04-18 16:45:55 -0500

    Testing push from IDE.

--- BLERobotController/ViewController.swift
+++ BLERobotController/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         //Bongo was here.
+        //Lets test from IDE.
     }
 
 
